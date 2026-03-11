efe1eb48271ace2e9f735a3516b45e093fa2d96e
md abu hanif mia <mohammadbitullah@gmail.com>
2026-03-11 16:15:18 +0600

    starting hero section

---
+++ src/pages/home/HeroSection.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div className='color-bg' >
+            
+         <div className='max-w-screen-2xl container max-auto py-20 px-5 ' >
+
+            <div>
+                <h1>left side</h1>
+            </div>
+
+            <div>
+                <h1>right side</h1>
+            </div>
+
+         </div>
+
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import HeroSection from './HeroSection';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home page</h1>
-        </div>
+        <> 
+            <HeroSection />
+            
+        </>
     );
 };
 
